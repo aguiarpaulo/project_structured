0511d9afb6d01f891cfd2a45f73ed6cdab0ead1e
Paulo Aguiar <aguiarlapaulo@gmail.com>
2023-10-23 17:12:02 -0300

    transform and load

---
+++ app/pipeline/load.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+def load_excel(data_frame:pd.DataFrame, output_path: str, file_name: str) -> str:
+
+    """
+    Save in a excel file
+    args:
+    data_frame(pd.DataFrame): dataframe to be saved
+    output_path(str): where the file should be saved
+    file_name(str): file name to be saved
+    """
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
+    return "File saved successfully"
---
+++ tests/test_pipeline.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from app.pipeline.transform import concat_data_frames
+
+df_1 = pd.DataFrame({'col1':[1,2],'col2':[3,4]})
+df_2 = pd.DataFrame({'col1':[5,6],'col2':[7,8]})
+
+def test_of_concat_data_frames():
+    # arrange
+    data_frame_list = [df_1,df_2]
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
+
+    # act
+    df = concat_data_frames(data_frame_list)
+    
+    # assert
+    assert df.shape == (4,2)
+    assert data_frame.equals(df)
