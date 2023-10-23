6957713d601c6db2f2d86d6e00988bb40a910d67
Paulo Aguiar <aguiarlapaulo@gmail.com>
2023-10-23 14:17:30 -0300

    extract config

---
+++ app/pipeline/extract.py
@@ -0,0 +1,23 @@
+import os #to manage files and folders
+import glob #to list files
+import pandas as pd
+from typing import List
+
+##Read files from data/input and bring a list of dataframes
+
+#args: input_path (str): path of the folder with files
+#return: list of dataframes
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+
+    data_frame_list = []
+    for file in all_files:
+        data = pd.read_excel(file)
+        data_frame_list.append(data)
+
+    return data_frame_list
+
+if __name__ == "__main__":
+    data_frame_list = extract_from_excel("data/input")
+    print(data_frame_list)
